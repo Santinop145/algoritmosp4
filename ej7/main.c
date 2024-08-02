da00a095be8c9d67b628085cc9cd5659877fb68a
Santinop145 <santinoalessandrop@gmail.com>
2024-08-02 10:23:58 -0300

    aabcp

--- ej7/main.c
+++ ej7/main.c
@@ -4,11 +4,13 @@
 #include <time.h>
 
 int main(void){
-    int arraysito[6];
+    int arraysito[40];
     
-    for(int i = 6; i >= 1; i--){
+    for(int i = 40; i >= 1; i--){
+        if(i % 2 == 0){
         arraysito[i] = i;
         printf("%d\n", arraysito[i]);
+        }
     }
     return 0;
 }
